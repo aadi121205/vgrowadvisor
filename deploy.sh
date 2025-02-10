7229d7f2f2e296213e5d62edc370190a6771a4b1
aadi <aaditya12120205@gmail.com>
2025-02-11 01:32:11 +0530

    Update

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo "Deploying Frontend"
-cd WritingDrive
 
 echo "Installing dependencies"
 npm install
@@ -12,8 +11,7 @@ npm run build
 echo "Deploying Frontend to VPS"
 
 # Using sshpass to automate the password entry for scp
-sshpass -p '31XucZe00WoY2F4W4u+T' scp -r dist/* root@88.222.213.64:/var/www/writingdrive.com
+sshpass -p '31XucZe00WoY2F4W4u+T' scp -r dist/* root@88.222.213.64:/var/www/vgrowadvisor.com
 
 echo "Frontend deployed successfully"
 
-cd ..
